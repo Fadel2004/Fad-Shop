ff70fc7b0be9e425e407663eb6a04854945f2b1d
M.Fadel Ndiaye <ndiayemouhamadoufadel@gmail.com>
2025-07-03 01:14:52 +0000

    Update vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -3,6 +3,7 @@ import react from '@vitejs/plugin-react';
 
 // https://vitejs.dev/config/
 export default defineConfig({
+   base: '/Fad-Shop/',
   plugins: [react()],
   optimizeDeps: {
     exclude: ['lucide-react'],
